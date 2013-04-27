10b72fdb01c73ce90d94968cb2b1805012805680
bradgearon@gmail.com <bradgearon@gmail.com>
2013-04-27 09:48:57 -0500

    Initial import

---
+++ src/BAG.UnityPackage.Console/Options.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerArgs;
+
+namespace BAG.UnityPackage.Cmd
+{
+    [TabCompletion]
+    [ArgExample("xup [-i] test.unitypackage [-o] test", "extracts test.unitypackage to the folder 'test'")]
+    public class Options
+    {
+        [ArgPosition(0), ArgRequired(PromptIfMissing = true), ArgExistingFile(), ArgShortcut("i")]
+        [ArgDescription("Input file, the file to extract")]
+        public string In { get; set; }
+
+        [ArgPosition(1), ArgShortcut("o")]
+        [ArgDescription("Output path, defaults to the current path and name of the input file")]
+        public string Out { get; set; }
+
+        [ArgShortcut("d")]
+        [ArgDescription("debug, true to launch debugger")]
+        public bool Debug { get; set; }
+    }
+}
---
+++ src/BAG.UnityPackage.Console/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerArgs;
+using SharpCompress.Archive;
+using System.Diagnostics;
+using System.IO;
+
+namespace BAG.UnityPackage.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var parsed = Args.Parse<Options>(args);
+                if (parsed.Debug)
+                {
+                    Debugger.Launch();
+                }
+                new UnityPackageMetadataProvider().Extract(parsed);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                ArgUsage.GetStyledUsage<Options>().Write();
+            }
+            catch (Exception ex)
+            {
+                ArgUsage.GetStyledUsage<Options>().Write();
+            }
+        }
+    }
+
+
+
+    interface IPackageMetadata
+    {
+        string path { get; }
+        string name { get; }
+
+        IQueryable<IPackageMetadata> items { get; }
+    }
+
+    static class UnityPackageExtensions
+    {
+        public static IQueryable<IPackageMetadata> Extract(this IPackageMetadata package)
+        {
+            return null;
+        }
+    }
+
+    public class UnityPackageMetadataProvider
+    {
+        // open the package depending on what type of archive it is
+        // use the appropriate metadata
+        public void Extract(Options options)
+        {
+            if (string.IsNullOrEmpty(options.Out))
+            {
+                options.Out = Path.GetFileNameWithoutExtension(options.In);
+            }
+            var archive = ArchiveFactory.Open(options.In);
+            Func<IArchiveEntry, string> getFirstLine = (entry) =>
+            {
+                var path = string.Empty;
+                using (var sr = new StreamReader(entry.OpenEntryStream()))
+                {
+                    path = sr.ReadLine();
+                }
+                return path;
+            };
+
+            if (archive != null)
+            {
+                foreach (var volume in archive.Entries.ToArray())
+                {
+                    var tempFile = Path.GetTempFileName();
+                    try
+                    {
+                        using (var tempStream = File.Open(tempFile, FileMode.Open))
+                        {
+                            volume.WriteTo(tempStream);
+                            tempStream.Flush();
+                        }
+
+                        Dictionary<string, string> toExtract = null;
+
+                        using (var tempArchive = ArchiveFactory.Open(tempFile))
+                        {
+                            if (tempArchive != null)
+                            {
+                                var pathEntries = from entry in tempArchive.Entries.ToArray()
+                                                  where Path.GetFileName(entry.FilePath).Contains("pathname") 
+                                                    && !entry.IsDirectory
+                                                  select entry; 
+                                toExtract = pathEntries.ToDictionary(
+                                    pathEntry => Path.GetDirectoryName(pathEntry.FilePath), 
+                                    pathEntry => getFirstLine(pathEntry));
+                            }
+                        }
+
+                        using (var tempArchive = ArchiveFactory.Open(tempFile))
+                        {
+                            if (tempArchive != null)
+                            {
+                                var assets = from entry in tempArchive.Entries.ToArray()
+                                             where Path.GetFileName(entry.FilePath).Contains("asset")
+                                                && !entry.IsDirectory
+                                             select new { 
+                                                 entry = entry, 
+                                                 path = toExtract[Path.GetDirectoryName(entry.FilePath)] };
+                                
+                                foreach (var asset in assets)
+                                {
+                                    var destPath = Path.Combine(Path.GetFullPath(options.Out),
+                                        asset.path.Replace('/', Path.DirectorySeparatorChar));
+
+                                    var destDir = Path.GetDirectoryName(destPath);
+                                    if (!Directory.Exists(destDir))
+                                    {
+                                        Directory.CreateDirectory(destDir);
+                                    }
+
+                                    asset.entry.WriteToFile(destPath);
+                                }
+                            }
+                        }
+                    }
+
+                    catch (Exception e)
+                    {
+                        if (options.Debug)
+                        {
+                            Console.Error.WriteLine(e.ToString());
+                        }
+                        throw;
+                    }
+
+                    finally
+                    {
+                        if (tempFile != null && File.Exists(tempFile))
+                        {
+                            File.Delete(tempFile);
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+}
